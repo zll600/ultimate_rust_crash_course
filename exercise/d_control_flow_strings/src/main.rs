195b230109d72a62c8a13af75a48a2a905f0d2ee
zll600 <3400692417@qq.com>
2022-07-15 08:46:03 +0800

    Complete d_exercise

--- exercise/d_control_flow_strings/src/main.rs
+++ exercise/d_control_flow_strings/src/main.rs
@@ -19,7 +19,13 @@ fn main() {
         // - If arg is "sum", then call the sum() function
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
-
+        if arg == "sum" {
+            sum();
+        } else if arg == "double" {
+            double();
+        } else {
+            count(arg);
+        }
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -31,7 +37,10 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
-
+    
+    for num in 7..=23 {
+        sum += num;
+    }
 
     println!("The sum is {}", sum);
 }
@@ -42,7 +51,11 @@ fn double() {
     // 3. Use a "while loop" to count how many times you can double the value of `x` (multiply `x`
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
-
+    
+    while x <= 500 {
+        count += 1;
+        x *= 2;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -52,6 +65,16 @@ fn count(arg: String) {
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
     // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+
+    let mut cnt = 1;
+    loop {
+        print!("{} ", arg);
+
+        if cnt >= 8 {
+            break;
+        }
+        cnt += 1;
+    }
 
 
     println!(); // This will output just a newline at the end for cleanliness.
